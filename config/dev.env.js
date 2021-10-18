abd15cd2ad848efeda899e433fa548a7e7cbc030
xieyi <yxie@nbai.io>
2021-10-18 17:48:01 +0800

    config

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  BASE_APIURL: '"http://192.168.88.217:8093"'
+  NODE_ENV: '"development"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   BASE_NETWORK: '"https://api.nbai.io"',
+  // BASE_APIURL: '"http://192.168.88.217:8093"',
   BASE_APIURL: '"https://api1.nbai.io"',
   BASE_SCANURL: '"https://scan.nbai.io/#/"'
 }
